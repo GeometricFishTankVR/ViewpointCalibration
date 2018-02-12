5ec75fbf0b056700b721980d705f68d5930fcbe4
Andrew <ajw659@mail.usask.ca>
2018-02-12 14:37:05 -0600

    updated the screen calibration to output transformation in a unity usable form

--- Matlab_Scripts/Screen_Calibration/save_matrix_to_xml.m
+++ Matlab_Scripts/Screen_Calibration/save_matrix_to_xml.m
@@ -1,6 +1,7 @@
 function save_matrix_to_xml( mat, filename )
 %SAVE_MATRIX_TO_XML Summary of this function goes here
 %   Detailed explanation goes here
+mat = matlab_to_unity_matrix_fix(mat);
 docNode = com.mathworks.xml.XMLUtils.createDocument('Matrix4x4');
 docRootNode = docNode.getDocumentElement;
 docRootNode.setAttribute('xmlns:xsd','http://www.w3.org/2001/XMLSchema');
@@ -18,6 +19,29 @@ end
 xmlFileName = [filename,'.xml'];
 xmlwrite(xmlFileName,docNode);
 type(xmlFileName);
+
+end
+
+function new_mat = matlab_to_unity_matrix_fix(mat)
+
+% Physical Dimension of the screens (im meters)
+screenWidthMeters = 0.196608;
+screenHeightMeters = 0.147456;
+
+% Reflection matrix to change hadedness
+handedness_fix = eye(4);
+handedness_fix(1,1) = -1;
+
+% Setup matrices to properly position unity quads based on the top left corner of the screens 
+pre_translation = eye(4);
+pre_translation(1,4) = - screenWidthMeters/2;
+pre_translation(2,4) = - screenHeightMeters/2;
+post_translation = eye(4);
+post_translation(1,4) = screenWidthMeters/2;
+post_translation(2,4) = screenHeightMeters/2;
+
+% Calculate new transformation matrix
+new_mat = handedness_fix * pre_translation * mat * post_translation * handedness_fix;
 
 end
 
